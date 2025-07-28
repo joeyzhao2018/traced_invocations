63efa88c45c6c0b45ee4e32647fcdd7a8c0f4551
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2025-07-28 16:57:58 -0400

    Initial commit

---
+++ test/traced_invocations.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TracedInvocations from '../lib/traced_invocations-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/traced_invocations-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TracedInvocations.TracedInvocationsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
